74b8707b147b3e3ac4a55e58f173122f58e5d581
lucas-perata <contacto.perata@gmail.com>
2022-02-27 16:21:45 -0300

    Selecciones de grid

--- myScript.js
+++ myScript.js
@@ -1,17 +1,123 @@
 // Loop para crear todos los DIVS en el HTML. 
 
-for (i = 0; i<256 ; i++){
-    const  container = document.getElementById("grid"); // Div Padre. 
+const  container = document.getElementById("grid"); // Padre
 
-    const div = document.createElement("div"); // Crear divs     
-    div.style.background = "grey"; 
+// 16*16  GRID 
 
-    div.style.border = "solid"    
-    container.appendChild(div);     // Introducir divs en #GRID  
+const button16 = document.querySelector(".grid16");
 
-    // Estilo 
-    container.style.gridTemplateColumns = "repeat(4, 1fr )"; 
-    container.style.gridTemplateRows = "repeat(64, 50px)"; 
+button16.addEventListener("click", grid16); 
+
+function grid16 (){
+
+    
+    for (i = 0; i<256 ; i++){
+
+        const div = document.createElement("div"); // Crear divs     
+        div.setAttribute("id", "grid1")
+        container.appendChild(div);     // Introducir divs en #GRID  
+    
+        // Estilo 
+        container.style.gridTemplateColumns = "repeat(16, 1fr )"; 
+        container.style.gridTemplateRows = "repeat(64, 50px)"; 
+      
+
+    }
+    
+}
+
+
+// 10*10 
+
+const button10 = document.querySelector(".grid10");
+
+button10.addEventListener("click", grid10); 
+
+function grid10 (){
+
+    
+    for (i = 0; i<100 ; i++){
+
+        const div = document.createElement("div"); // Crear divs     
+        
+        container.appendChild(div);     // Introducir divs en #GRID  
+        div.setAttribute("id", "grid1")
+        // Estilo 
+        container.style.gridTemplateColumns = "repeat(10, 1fr )"; 
+        container.style.gridTemplateRows = "repeat(10, 50px)"; 
+    
+    }
 
 }
+
+
+
+
+// Boton limpiar
+
+const clean = document.querySelector(".clear")
+
+clean.addEventListener("click", limpiar); 
+
+function limpiar(){
+    console.log("limpio"); 
+
+    const gridLimpio = document.querySelectorAll("#grid1").forEach( gridLimpio => gridLimpio.setAttribute("class", "normal"))
+
+    eleccionG()
+}
+
+// Eleccion del grid
+
+
+function eleccionG(){
+
+    const eleccion = window.prompt("10 o 16"); 
+
+    if (eleccion == 16){
+        for (i = 0; i<256 ; i++){
+
+            const div = document.createElement("div"); // Crear divs     
+            div.setAttribute("id", "grid1")
+            container.appendChild(div);     // Introducir divs en #GRID  
+        
+            // Estilo 
+            container.style.gridTemplateColumns = "repeat(16, 1fr )"; 
+            container.style.gridTemplateRows = "repeat(64, 50px)"; 
+          // Mouse event - pintar 
+
+
+        const grid1 = document.querySelectorAll("#grid1").forEach(grid1 => grid1.addEventListener("click" , cambios) ) 
+
+        function cambios(){
+        this.setAttribute("class", "black")
+    
+        }
+        
+    }
+}
+
+    else {
+        for (i = 0; i<100 ; i++){
+
+            const div = document.createElement("div"); // Crear divs     
+            div.setAttribute("id", "grid1")
+            container.appendChild(div);     // Introducir divs en #GRID  
+        
+            // Estilo 
+            container.style.gridTemplateColumns = "repeat(10, 1fr )"; 
+            container.style.gridTemplateRows = "repeat(10, 50px)"; 
+            
+            // Mouse event - pintar 
+
+
+        const grid1 = document.querySelectorAll("#grid1").forEach(grid1 => grid1.addEventListener("click" , cambios) ) 
+
+        function cambios(){
+        this.setAttribute("class", "black")
+}   
+        }
+    }
+}
+
 
